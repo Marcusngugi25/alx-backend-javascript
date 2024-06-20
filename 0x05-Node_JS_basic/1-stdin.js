88e18b6d538fd558f1ad0b97394ca570f496918e
Marcusngugi25 <marcusngugi@gmail.com>
2024-06-20 13:24:01 -0700

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,26 @@
-// Print a welcome message to the standard output
+/**
+ * Prompts the user to enter their name, reads the input from STDIN,
+ * and displays a message containing the user's name.
+ *
+ * When the user ends the program, a closing message is displayed.
+ *
+ * @example
+ * // Output:
+ * // Welcome to Holberton School, what is your name?
+ * // Alice
+ * // Your name is: Alice
+ * // This important software is now closing
+ */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Check if the standard input is a TTY (terminal)
-if (process.stdin.isTTY) {
-  // If the standard input is a TTY, listen for data events
-  process.stdin.on('data', (data) => {
-    // Print the received data along with a message to the standard output
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    // Exit the process
-    process.exit();
-  });
-} else {
-  // If the standard input is not a TTY, listen for data events
-  process.stdin.on('data', (data) => {
-    // Print the received data along with a message to the standard output
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    // Exit the process
-    process.exit();
-  });
-  // When the process is about to exit, execute the callback function
-  process.on('exit', () => {
-    // Print a closing message to the standard output
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
